df85384e5db2b895b32029f3b1ad9500a7360dd4
KrystynaSyty <153935914+KrystynaSyty@users.noreply.github.com>
2025-07-01 15:50:57 +0200

    Update properties.py

--- src/mbe_automation/properties.py
+++ src/mbe_automation/properties.py
@@ -11,6 +11,7 @@ import os.path
 import torch
 import mace.calculators
 import mbe_automation.structure.relax
+from phonopy.qha import QHA
 
 def phonons_from_finite_differences(
         unit_cell,
