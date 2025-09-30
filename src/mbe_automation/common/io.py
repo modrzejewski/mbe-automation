cc586beb0356997153ac3867d7403216352ff3c3
Marcin Modrzejewski <m.modrzejewski@uw.edu.pl>
2025-09-30 23:43:09 +0200

    symmetry in cif writer

--- src/mbe_automation/common/io.py
+++ src/mbe_automation/common/io.py
@@ -19,7 +19,16 @@ def read(file_path: str) -> ase.Atoms:
 
 def write(
         file_path: str,
-        system: ase.Atoms
-):    
-    ase.io.write(file_path, system)
+        system: ase.Atoms,
+        symprec=1.0E-5
+):
+    if file_path.lower().endswith(".cif"):
+        pmg_structure = pymatgen.io.ase.AseAtomsAdaptor.get_structure(system)
+        cif_writer = pymatgen.io.cif.CifWriter(pmg_structure, symprec=symprec, refine_struct=False)
+        cif_writer.write_file(file_path)
+    else:
+        ase.io.write(file_path, system)
     
+
+
+        
