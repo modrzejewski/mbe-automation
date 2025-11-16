0fe4e4097309f473d34c4750d17a8fa9d1a27259
Marcin Modrzejewski <m.modrzejewski@uw.edu.pl>
2025-11-16 13:53:03 +0100

    templates for slurm gfn-xtb

---
+++ templates/queue-scripts/Poznań/gfn2-xtb-cpu.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+#SBATCH --job-name="GFN2-xTB"
+#SBATCH -A pl0415-02
+#SBATCH -p altair 
+#SBATCH --nodes 1      
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=48
+#SBATCH --time=24:00:00
+#SBATCH --mem=180gb
+
+import os
+import os.path
+import sys
+import subprocess
+
+InpScript = "{inp_script}"
+LogFile = "{log_file}"
+#
+# Set up virtual environment
+# Make sure that the environment works properly on the coputer nodes.
+# The libaries seen from the login nodes and compute nodes can be
+# different!
+# It's a good idea to log on to a compute node via an interactive slurm
+# session and create a venv from within this session.
+#
+virtual_environment = os.path.expanduser("~/.virtualenvs/compute-env")
+virtual_environment = os.path.realpath(virtual_environment)
+activate_env = os.path.realpath(os.path.join(virtual_environment, "bin", "activate"))
+cmd = f"module load oneAPI python/3.11.9-gcc-11.5.0-5l7rvgy && . {{activate_env}} && python {{InpScript}}"
+
+with open(LogFile, "w") as log_file:
+    process = subprocess.Popen(cmd, shell=True, stdout=log_file,
+                               stderr=subprocess.STDOUT, bufsize=1,
+                               universal_newlines=True)
+    process.communicate()
+    
+
+
+    
+
+
+
