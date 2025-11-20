d7217e1b84d9db4058958e05103ef483df0243a1
Marcin Modrzejewski <m.modrzejewski@uw.edu.pl>
2025-11-20 21:13:47 +0100

    dftb+

--- src/mbe_automation/calculators/dftb.py
+++ src/mbe_automation/calculators/dftb.py
@@ -100,7 +100,7 @@ class DFTBCustom(Dftb):
             "Driver_Optimizer": "LBFGS {}",
             "Driver_MovedAtoms": "1:-1",
             "Driver_Convergence_": "",
-            "Driver_GradElem [eV/Angstrom]": max_force_on_atom,
+            "Driver_Convergence_GradElem [eV/Angstrom]": max_force_on_atom,
             "Driver_MaxSteps": max_steps,
             "Driver_LatticeOpt": "Yes" if optimize_lattice_vectors else "No",
             "Driver_AppendGeometries": "No"
