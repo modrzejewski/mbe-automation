ebaa38e0b5c6b7543eddbcf9a8e16abf6de6c71c
Marcin Modrzejewski <m.modrzejewski@uw.edu.pl>
2025-11-20 18:21:18 +0100

    corrected calculators/__init__.py

--- src/mbe_automation/calculators/__init__.py
+++ src/mbe_automation/calculators/__init__.py
@@ -1,6 +1,6 @@
 from . import dftb
 from . import tblite
-from dftb import GFN1_xTB, GFN2_xTB, DFTB_Plus_MBD, DFTB3_D4
+from .dftb import GFN1_xTB, GFN2_xTB, DFTB_Plus_MBD, DFTB3_D4
 
 __all__ = [
     "GFN1_xTB",
