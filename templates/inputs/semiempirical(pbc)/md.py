891f3d26bafd97ae6a8cc6dc8d18071949122ab6
Marcin Modrzejewski <m.modrzejewski@uw.edu.pl>
2025-11-21 09:44:59 +0100

    template for md with semiempirical methods

---
+++ templates/inputs/semiempirical(pbc)/md.py
@@ -0,0 +1,46 @@
+import numpy as np
+import os.path
+
+import mbe_automation
+import mbe_automation.configs
+import mbe_automation.workflows
+from mbe_automation.storage import from_xyz_file
+
+xyz_solid = "{xyz_solid}"
+xyz_molecule = "{xyz_molecule}"
+work_dir = os.path.abspath(os.path.dirname(__file__))
+
+env_config = ParallelCPU.recommended(model_name="dftb3-d4")
+env_config.set()
+
+molecule = from_xyz_file(os.path.join(work_dir, xyz_molecule))
+crystal = from_xyz_file(os.path.join(work_dir, xyz_solid))
+
+md_config = mbe_automation.configs.md.Enthalpy(
+    molecule = molecule,
+    crystal = crystal,
+    calculator = mbe_automation.calculators.DFTB3_D4(elements=crystal.symbols),
+    temperature_K = 298.15,
+    pressure_GPa = 1.0E-4,
+    work_dir = work_dir,
+    dataset = os.path.join(work_dir, "properties.hdf5"),
+    
+    md_molecule = mbe_automation.configs.md.ClassicalMD(
+        ensemble = "NVT",
+        time_total_fs = 10000.0,
+        time_step_fs = 1.0,
+        sampling_interval_fs = 50.0,
+        time_equilibration_fs = 5000.0,
+    ),
+    
+    md_crystal = mbe_automation.configs.md.ClassicalMD(
+        ensemble = "NPT",
+        time_total_fs = 10000.0,
+        time_step_fs = 1.0,
+        sampling_interval_fs = 50.0,
+        time_equilibration_fs = 5000.0,
+        supercell_radius = 15.0,
+    )
+)
+
+mbe_automation.workflows.md.run(md_config)
--- templates/inputs/semiempirical(pbc)/quasi_harmonic.py
+++ templates/inputs/semiempirical(pbc)/quasi_harmonic.py
@@ -3,26 +3,27 @@ import os.path
 import mbe_automation
 from mbe_automation.storage import from_xyz_file
 from mbe_automation.configs.execution import ParallelCPU
+from mbe_automation.configs.structure import Minimum
 
 xyz_solid = "{xyz_solid}"
 xyz_molecule = "{xyz_molecule}"
 work_dir = os.path.abspath(os.path.dirname(__file__))
 
-env_config = ParallelCPU.recommended(model_name="gfn2-xtb")
+env_config = ParallelCPU.recommended(model_name="dftb3-d4")
 env_config.set()
 
+crystal = from_xyz_file(xyz_solid)
+molecule = from_xyz_file(xyz_molecule)
+
 properties_config = mbe_automation.configs.quasi_harmonic.FreeEnergy.recommended(
-    model_name = "gfn2-xtb",
-    crystal = from_xyz_file(os.path.join(work_dir, xyz_solid)),
-    molecule = from_xyz_file(os.path.join(work_dir, xyz_molecule)),
+    model_name = "dftb3-d4",
+    crystal = crystal,
+    molecule = molecule,
     thermal_expansion = False,
-    calculator = mbe_automation.calculators.GFN2_xTB(verbose=True),
+    calculator = mbe_automation.calculators.DFTB3_D4(elements=crystal.symbols),
     supercell_radius = 10.0,
-    work_dir = os.path.join(work_dir, "properties"),
-    dataset = os.path.join(work_dir, "properties.hdf5")
+    work_dir = work_dir,
+    dataset = os.path.join(work_dir, "properties.hdf5"),
 )
 
 mbe_automation.workflows.quasi_harmonic.run(properties_config)
-
-
-
