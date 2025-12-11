59565ffa9833bf73ddaabbfb37e87b0ac2ffd31d
Marcin Modrzejewski <m.modrzejewski@uw.edu.pl>
2025-12-11 16:26:59 +0100

    another typo

--- src/mbe_automation/api/classes.py
+++ src/mbe_automation/api/classes.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
 from typing import Tuple, Literal, Sequence, List
+from pathlib import Path
 import numpy as np
 import numpy.typing as npt
 from mace.calculators import MACECalculator
