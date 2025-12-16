0011a4a9378b70456024479f3ab80403ac6a1161
Marcin Modrzejewski <m.modrzejewski@uw.edu.pl>
2025-12-16 19:33:18 +0100

    typo

--- src/mbe_automation/configs/md.py
+++ src/mbe_automation/configs/md.py
@@ -150,6 +150,11 @@ class ClassicalMD:
                                    #
     supercell_diagonal: bool = False
 
+    def __post_init__(self):
+        assert self.sampling_interval_fs > self.time_step_fs
+        assert self.time_total_fs > self.sampling_interval_fs
+        assert self.time_total_fs > self.time_equilibration_fs
+
 @dataclass(kw_only=True)
 class Enthalpy:
                                    #
@@ -212,10 +217,6 @@ class Enthalpy:
             self.crystal = mbe_automation.storage.to_ase(self.crystal)
         if isinstance(self.molecule, mbe_automation.storage.Structure):
             self.molecule = mbe_automation.storage.to_ase(self.molecule)
-
-        assert self.sampling_interval_fs > self.time_step_fs
-        assert self.time_total_fs > self.sampling_interval_fs
-        assert self.time_total_fs > self.time_equilibration_fs
         
         self.temperatures_K = np.atleast_1d(self.temperatures_K)
         self.pressures_GPa = np.atleast_1d(self.pressures_GPa)
