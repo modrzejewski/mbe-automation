aa845960c86adeef10b5b4b93b7f3cfabe3cd8da
Marcin Modrzejewski <m.modrzejewski@uw.edu.pl>
2025-12-22 00:31:07 +0100

    corrected type hinting

--- src/mbe_automation/calculators/__init__.py
+++ src/mbe_automation/calculators/__init__.py
@@ -2,11 +2,11 @@ from . import dftb
 from . import tblite
 from . import core
 
-from .dftb import GFN1_xTB, GFN2_xTB, DFTB_Plus_MBD, DFTB3_D4
-from .pyscf import DFT, HF
+from .dftb import DFTBCalculator, GFN1_xTB, GFN2_xTB, DFTB_Plus_MBD, DFTB3_D4
+from .pyscf import PySCFCalculator, DFT, HF
 from .mace import MACE
 from .core import run_model
 from .isolated_atoms import atomic_energies
 from . import pyscf
 
-CALCULATORS = GFN1_xTB | GFN2_xTB | DFTB_Plus_MBD | DFTB3_D4 | DFT | HF | MACE
+CALCULATORS = PySCFCalculator | DFTBCalculator | MACE
