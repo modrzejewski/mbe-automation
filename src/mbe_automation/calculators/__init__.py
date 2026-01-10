714cb524143d3785cb9022b07454c1a5bc4db419
Marcin Modrzejewski <46387219+modrzejewski@users.noreply.github.com>
2026-01-10 13:30:34 +0100

    Import DeltaMACE in __init__.py

--- src/mbe_automation/calculators/__init__.py
+++ src/mbe_automation/calculators/__init__.py
@@ -4,7 +4,7 @@ from . import core
 
 from .dftb import DFTBCalculator, GFN1_xTB, GFN2_xTB, DFTB_Plus_MBD, DFTB3_D4
 from .pyscf import PySCFCalculator, DFT, HF
-from .mace import MACE
+from .mace import MACE, DeltaMACE
 from .core import run_model
 from .core import CALCULATORS
 from .isolated_atoms import atomic_energies
